6af8a60c39892705e78b96b66c081cc40c09829d
una <unagamix@gmail.com>
2023-01-09 23:13:52 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,88 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 import * as dotenv from 'dotenv';
 
+type MusicDetail = {
+  level: number;
+  difficulty:
+    | 'NOV'
+    | 'ADV'
+    | 'EXH'
+    | 'MXM'
+    | 'INF'
+    | 'GRV'
+    | 'HVN'
+    | 'VVD'
+    | 'XCD';
+  jacket: string;
+};
+
+type Music = {
+  title: string;
+  artist: string;
+  details: MusicDetail[];
+  tags: string[];
+  pack: string;
+};
+
+type InitialMusic = Omit<Music, 'details'>;
+
 dotenv.config();
 
+const BASE_URL = `${process.env.BASE_URL}`;
+const INDEX_URL = `${BASE_URL}${process.env.INDEX_PATH}`;
+
+const fetchHtml = async (url: string): Promise<string> => {
+  const { data: html } = await axios.get(url);
+  return html;
+};
+
+const parseCheerioDom = (html: string): cheerio.CheerioAPI => {
+  return cheerio.load(html);
+};
+
+const parseDetailUrls = ($: cheerio.CheerioAPI): string[] => {
+  return $('a.detail_pop')
+    .toArray()
+    .map(({ attribs }) => `${BASE_URL}${attribs['href']}`);
+};
+
+const fetchDetailFormUrl = async (
+  url: string,
+  music: InitialMusic
+): Promise<Music> => {
+  const $ = parseCheerioDom(await fetchHtml(url));
+  const details = $('div.cat')
+    .map((_, el) => {
+      const jacket = $(el).find('div.jk img').first().attr('src');
+      const difficulty = $(el).find('p').first();
+      return {
+        level: Number(difficulty.html()),
+        difficulty: difficulty.attr('class')?.toUpperCase() || '',
+        jacket: `${BASE_URL}${jacket}`,
+      } as MusicDetail;
+    })
+    .toArray();
+  return {
+    ...music,
+    details,
+  };
+};
+
 const main = async () => {
-  const { data: html } = await axios.get(process.env.BASE_URL || '');
-  const $ = cheerio.load(html);
+  const $ = parseCheerioDom(await fetchHtml(INDEX_URL));
   const maxPage = $('select#search_page > option').last().val();
   console.log('#', { maxPage });
+  //fetchDetailUrls($);
+  const r = await fetchDetailFormUrl(
+    'https://p.eagate.573.jp/game/eacsdvx/vi/music/detail.html?music_id=h9gcpJ23dTi67XZRo2okkg',
+    {
+      title: 'dummy',
+      artist: 'aaa',
+      tags: [],
+      pack: 'no',
+    }
+  );
+  console.log('#', { r: JSON.stringify(r) });
 };
 
 main();
