e08244b0df1f9affab3d11482dd662ef8c5bfb8d
una <unagamix@gmail.com>
2023-01-11 23:07:18 +0900

    fix

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
 # sdvx-ecloud-data-scraping
+
+- This tool is scraping for music data by SOUND VOLTEX EXCEED GEAR.
+
+## Usage
+
+```bash
+$ yarn install
+$ yarn start
+```
+
+## Output Example JSON
+
+```json
+[
+  {
+    "title": "Amazing Mirage",
+    "artist": "lapix",
+    "pack": "コナステ版 SOUND VOLTEX beatmania IIDX セレクション 楽曲パック vol.1",
+    "tags": [
+      "BEMANI"
+    ],
+    "details": [
+      {
+        "level": 5,
+        "difficulty": "NOV",
+        "jacket": "/game/sdvx/vi/common/jacket.html?img=ONTYekdFQAkIlvI7DP21ow"
+      },
+      {
+        "level": 12,
+        "difficulty": "ADV",
+        "jacket": "/game/sdvx/vi/common/jacket.html?img=bxtUcasAa_oKpmpA9MS5-Q"
+      },
+      {
+        "level": 15,
+        "difficulty": "EXH",
+        "jacket": "/game/sdvx/vi/common/jacket.html?img=x5oEVjmWSLewiFoME3xdHw"
+      },
+      {
+        "level": 18,
+        "difficulty": "MXM",
+        "jacket": "/game/sdvx/vi/common/jacket.html?img=L-HKXEdNbUnvWBFkLak9Hw"
+      }
+    ]
+  },
+  {
+    "title": "Element of SPADA",
+    "artist": "猫叉Master feat.霜月はるか",
+    "pack": "コナステ版 SOUND VOLTEX beatmania IIDX セレクション 楽曲パック vol.1",
+    "tags": [
+      "BEMANI"
+    ],
+    "details": [
+      {
+        "level": 5,
+        "difficulty": "NOV",
+        "jacket": "/game/sdvx/vi/common/jacket.html?img=Yyu1qFC3WRxpum-UQR49Fw"
+      },
+      {
+        "level": 11,
+        "difficulty": "ADV",
+        "jacket": "/game/sdvx/vi/common/jacket.html?img=vvNGJM8fE2I1h0WPN2HD4A"
+      },
+      {
+        "level": 14,
+        "difficulty": "EXH",
+        "jacket": "/game/sdvx/vi/common/jacket.html?img=r8Mm3MgIEC72xZUTmcKOjQ"
+      },
+      {
+        "level": 17,
+        "difficulty": "MXM",
+        "jacket": "/game/sdvx/vi/common/jacket.html?img=VT5T5LtkwOkxq3l2X2KfQg"
+      }
+    ]
+  },
+  ...
+```
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import axios from 'axios';
+import fs from 'fs';
 import * as cheerio from 'cheerio';
 import { MusicDetail, Music, MusicInitial } from './types';
 import { envs } from './envs';
+
+const OUTPUT_DIR = 'dist';
+const OUTPUT_PATH = `${OUTPUT_DIR}/output.json`;
 
 axios.defaults.baseURL = envs.BASE_URL;
 
 const fetchHtml = async (url: string): Promise<string> => {
   const { data: html } = await axios.get(url);
+  return html;
+};
+
+const fetchIndex = async (url: string, pageIndex: number): Promise<string> => {
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
+  const body = { page: pageIndex };
+  const { data: html } = await axios.post(url, body, {
+    headers,
+  });
   return html;
 };
 
@@ -55,16 +70,24 @@ const fetchDetailFormUrl = async ({
 };
 
 const main = async () => {
-  const $ = parseCheerioDom(await fetchHtml(envs.INDEX_PATH));
-  const maxPage = $('select#search_page > option').last().val();
-  const result = await Promise.all(
-    parseMusicList($).map((it) => {
-      return it;
-      //return fetchDetailFormUrl(it);
-    })
-  );
-
-  console.log('#', { maxPage, result });
+  const $ = parseCheerioDom(await fetchIndex(envs.INDEX_PATH, 0));
+  const maxPage = Number($('select#search_page > option').last().val());
+  const result: Music[] = [];
+  for (let i = 1; i <= maxPage; ++i) {
+    const dom = parseCheerioDom(await fetchIndex(envs.INDEX_PATH, i));
+    const musicPromise = parseMusicList(dom).map((it) =>
+      fetchDetailFormUrl(it)
+    );
+    for (let j = 0; j < musicPromise.length; ++j) {
+      const music = await musicPromise[j];
+      result.push(music);
+    }
+    console.log(`processing... ${i}/${maxPage}`);
+  }
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(OUTPUT_PATH, JSON.stringify(result, null, 2));
 };
 
 main();
