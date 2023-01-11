e05baa8a79a34b82c00d48b3b880bf59070a55fd
una <unagamix@gmail.com>
2023-01-11 23:34:57 +0900

    fix

--- globals.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    BASE_URL: string | undefined;
-    TOP_PAGE: string | undefined;
-    NODE_ENV: 'development' | 'production' | 'test';
-  }
-}
